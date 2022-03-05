d3009a6d5057c76b058d2ff6536ab3fc009edc21
Tommy <tikcheung3@gmail.com>
2022-03-06 02:52:40 +0800

    Position gain and loss is now working (with animation)

--- Assets/Javascript/position_change.js
+++ Assets/Javascript/position_change.js
@@ -1,103 +1,65 @@
-/*Ugly*/
-const Position_Box01 = document.getElementById("Position_Box01");
-const Position_Box02 = document.getElementById("Position_Box02");
-const Position_Box03 = document.getElementById("Position_Box03");
-const Position_Box04 = document.getElementById("Position_Box04");
-const Position_Box05 = document.getElementById("Position_Box05");
-const Position_Box06 = document.getElementById("Position_Box06");
-const Position_Box07 = document.getElementById("Position_Box07");
-const Position_Box08 = document.getElementById("Position_Box08");
-const Position_Box09 = document.getElementById("Position_Box09");
-const Position_Box10 = document.getElementById("Position_Box10");
-const Position_Box11 = document.getElementById("Position_Box11");
-const Position_Box12 = document.getElementById("Position_Box12");
-const Position_Box13 = document.getElementById("Position_Box13");
-const Position_Box14 = document.getElementById("Position_Box14");
-const Position_Box15 = document.getElementById("Position_Box15");
-const Position_Box16 = document.getElementById("Position_Box16");
-const Position_Box17 = document.getElementById("Position_Box17");
-const Position_Box18 = document.getElementById("Position_Box18");
-const Position_Box19 = document.getElementById("Position_Box19");
-const Position_Box20 = document.getElementById("Position_Box20");
 const Position_Gain_Input = document.getElementById("Position_Gain_Input");
 const Position_Loss_Input = document.getElementById("Position_Loss_Input");
+const SubmitButton01 = document.getElementById("submitButton1");
+const SubmitButton02 = document.getElementById("submitButton2");
 
 
 
-
-function GetPositionValue() {
-
+function Position_Gain() {
 
     let RawPositionGain = Position_Gain_Input.value;
-    let RawPositionLoss = Position_Gain_Input.value;
-    console.log(RawPositionLoss);
-    console.log(RawPositionGain);
+    let PositionGain = 0;
 
-    // let PositionGain = 0;
-    // let PositionLoss = 0;
+    if (RawPositionGain < 10 && RawPositionGain > 0) {
+        PositionGain = "0" + RawPositionGain;
+    }
+    else {
+        PositionGain = RawPositionGain;
+    }
 
+    document.getElementById("Position_Box" + PositionGain).classList.remove("Lose-place");
+    document.getElementById("Position_Box" + PositionGain).classList.add("Gain-place");
 
-    // if (RawPositionGain < 10) {
-    //     PositionGain = "0" + RawPositionGain;
-    // }
+    /*To do the Background-color disappearing animation;*/
+    setTimeout(function() {
+        document.getElementById("Position_Box" + PositionGain).classList.add("Gain-place-Animation");
+        document.getElementById("Position_Box" + PositionGain).classList.remove("Gain-place");}, 2600);
 
-
-
-    // if (RawPositionLoss < 10) {
-    //     PositionLoss = "0" + RawPositionLoss;
-    // }
-
-
-
-
+    setTimeout(function() {
+        document.getElementById("Position_Box" + PositionGain).classList.remove("Gain-place-Animation");}, 3200);
 
 
-    // Which_Position.classList.remove("Lose-place");
-    // Which_Position.classList.add("Gain-place");
-    // Position_Box05.classList.add("Gain-place");
-
-    
-    // setTimeout(function() {
-    //     Which_Position.classList.remove("Gain-place");}, 3000);
-
-//     Position_Gain(PositionGain);
-//     Position_Loss(PositionLoss);
 }
 
-Position_Box07.classList.add("Gain-place");
 
 
-// function Position_Gain(PositionGain) {
+function Position_Loss() {
 
-//     let Which_Position = "Position_Box" + PositionGain;
+    let RawPositionLoss = Position_Loss_Input.value;
+    let PositionLoss = "";
 
-//     Which_Position.classList.remove("Lose-place");
-//     Which_Position.classList.add("Gain-place");
-//     Position_Box05.classList.add("Gain-place");
+    if (RawPositionLoss < 10 && RawPositionLoss > 0) {
+        PositionLoss = "0" + RawPositionLoss;
+    }
+    else {
+        PositionLoss = RawPositionLoss;
+    }
 
-    
-//     setTimeout(function() {
-//         Which_Position.classList.remove("Gain-place");}, 3000);
-
-
-// }
-
-
-
-// function Position_Loss(PositionLoss) {
-
-//     let Which_Position = "Position_Box" + PositionLoss;
-//     console.log(Which_Position);
+    document.getElementById("Position_Box" + PositionLoss).classList.remove("Gain-place");
+    document.getElementById("Position_Box" + PositionLoss).classList.add("Lose-place");
 
 
-//     Position_Box.classList.remove("Gain-place");
-//     Position_Box.classList.add("Lose-place");
 
-//     setTimeout(function() {
-//         Which_Position.classList.remove("Lose-place");}, 3000);
+    /*To do the Background-color disappearing animation;*/
+    setTimeout(function() {
+        document.getElementById("Position_Box" + PositionLoss).classList.add("Lose-place-Animation");}, 2600);
+
+    setTimeout(function() {
+        document.getElementById("Position_Box" + PositionLoss).classList.remove("Lose-place");
+        document.getElementById("Position_Box" + PositionLoss).classList.remove("Lose-place-Animation");}, 3200);
 
 
-// }
+}
 
-Position_Gain_Input.addEventListener("onchange", GetPositionValue());
-Position_Loss_Input.addEventListener("change", GetPositionValue());
+SubmitButton01.addEventListener("click", Position_Gain);
+SubmitButton02.addEventListener("click", Position_Loss);
