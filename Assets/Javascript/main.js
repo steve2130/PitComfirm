d667b7e0418db6544a9c751ffdab62ca4ef3bc45
Tom <tikcheung3@gmail.com>
2022-04-21 15:52:55 +0800

    2022/04/21 HTML: - Changed everything In the nav bar
    JS:
    - Changed function related to nav bar
    - Changed the timezone for the clock
    - Changed the method of getting the city of the track based
    - Added method on getting status of livetiming (websocket) and race result
    
    CSS:
    - Changed styles related to the nav bar
    
    MISC:
    - Menu_symbol.svg is now included in the html file
    - Added index for Track_image.png (?)

--- Assets/Javascript/main.js
+++ Assets/Javascript/main.js
@@ -1,44 +1,45 @@
 /*For nav toggle expand / retract*/
 
-const navButton = document.getElementById("nav_button");
-const navBar = document.getElementById("navBar");
-const navList = document.querySelectorAll("#navList");
-const CircuitImage = document.getElementById("circuit_image");
-
-navButton.addEventListener("change", x => {
+const NavButton = document.getElementById("nav_button");
+const NavSideBar = document.getElementById("nav_side_bar");
+const NavSideBarWrapper = document.getElementById("nav_side_bar_wrapper");
+const NavList = document.querySelectorAll("#navList");
 
 
 
-    if (navButton.checked) {
-        navBar.style.height = "100px";
-        CircuitImage.style.height= "40px";
 
-        for (let i = 0, length = navList.length; i < length; i++) {
-            navList[i].classList.replace("nav_list", "nav_list_expanded");
-        }
+NavButton.addEventListener("click", x => {
+    NavSideBar.classList.replace("NavSideBar_Collapsed", "NavSideBar_Expanded");
+    NavSideBarWrapper.classList.replace("NavSideBarWrapper_Collapsed", "NavSideBarWrapper_Expanded");
+}, false);
+
+NavSideBar.addEventListener("click", (e) => {
+    /*PsiKai, https://stackoverflow.com/questions/69773505/prevent-overlay-click-for-elements-with-a-higher-z-index */
+    if (e.target.classList.contains("NavSideBar")) {
+        NavSideBar.classList.replace("NavSideBar_Expanded", "NavSideBar_Collapsed");
+        NavSideBarWrapper.classList.replace("NavSideBarWrapper_Expanded", "NavSideBarWrapper_Collapsed");
     }
 
-    else {
-        navBar.style.height = "38px";
-        CircuitImage.style.height= "20px";
-
-        for (let i = 0, length = navList.length; i < length; i++) {
-            navList[i].classList.replace("nav_list_expanded", "nav_list");
-        }
-    }
 }, false);
 
 /*__________________________________________________________________________________________ */
+/*Detect height changes on the country text*/
 
+const heightObserver = new ResizeObserver ((entries) => {
+    console.log(entries)
+})
+
+const CountriesText = document.getElementById("circuit_country_text");
+heightObserver.observe(CountriesText);
+/*__________________________________________________________________________________________ */
 
     /*For the clock in nav bar*/
     function GETDateandTime() {
         let date = new Date();
 
-        let option = { hour12: false };
-        let HKT = "zh-HK"
+        let option = { timeZone: 'UTC', hour12: false };
 
-        let LocalDate = date.toLocaleTimeString(HKT, option);
+        let LocalDate = date.toLocaleTimeString("en-GB", option);
         
 
         const timeCounter = document.getElementById("timeCounter");
@@ -64,8 +65,7 @@ navButton.addEventListener("change", x => {
                 .get(cors_bypass + 'https://api.formula1.com/v1/event-tracker', {
                     method: 'GET',
                     headers: {
-                        'apikey': 'qPgPPRJyGCIPxFT3el4MF7thXHyJCzAP',
-                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0',
+                        'apikey': 'qPgPPRJyGCIPxFT3el4MF7thXHyJCzAP',   /*Sniff your own*/
                         'Connection': 'keep-alive',
                         'Origin': 'https://www.formula1.com/',
                         'Referer': 'https://www.formula1.com/',
@@ -80,22 +80,16 @@ navButton.addEventListener("change", x => {
         }
     }
 
-    GetData().then(res => {
-        console.log(res);
-    });
-
 
 
         function GetCircultImage(eventData) {
-            console.log(eventData);
 
-            let circuit_image_title = eventData.race.meetingCountryName;
+            let circuit_image_title = eventData.circuitSmallImage.title;
             let circuit_image_url = eventData.circuitSmallImage.url;
 
-            console.log(circuit_image_title);
-            console.log(circuit_image_url);
+            circuit_image_title = circuit_image_title.split(".")
 
-            return [circuit_image_title, circuit_image_url];
+            return [circuit_image_title[0], circuit_image_url];
         }
 
         function GetCountryTimezoneOffset(eventData) {
@@ -118,6 +112,7 @@ navButton.addEventListener("change", x => {
 
         }
 
+        const CircuitImage = document.getElementById("circuit_image");
 
         async function happystuff() {
             let return_info = await GetData();
@@ -137,7 +132,79 @@ navButton.addEventListener("change", x => {
 
 
 
-        document.addEventListener('DOMContentLoaded', (event) => {
-            happystuff();
-            navButton.checked = false;  /*Love you firefox */
-        });
+/*__________________________________________________________________________________________*/
+
+        async function CheckWebsocketOpen() {
+            /*To check whether livetiming is available or not.*/
+            const cors_bypass = "https://cors-anywhere-proxy-fork.herokuapp.com/"; 
+            try {
+                let res = await axios
+                    .get(cors_bypass + 'https://livetiming.formula1.com/static/StreamingStatus.json', {
+                        method: 'GET',
+                        headers: {
+                            'Connection': 'keep-alive',
+                            'Host': 'livetiming.formula1.com',
+                            'Origin': 'https://www.formula1.com/',
+                            'Referer': 'https://www.formula1.com/',
+                        }
+                    })
+                    return res.data;
+            }
+    
+            catch (error) {
+                console.log(error);
+            }
+        }
+
+        async function GetRaceResult() {
+            /*To check whether livetiming is available or not.*/
+            const cors_bypass = "https://cors-anywhere-proxy-fork.herokuapp.com/"; 
+            try {
+                let res = await axios
+                    .get(cors_bypass + `https://livetiming.formula1.com/static/2022/2022-04-10_Australian_Grand_Prix/2022-04-10_Race/SPFeed.json`, {
+                        method: 'GET',
+                        headers: {
+                            'Connection': 'keep-alive',
+                            'Host': 'livetiming.formula1.com',
+                            'Origin': 'https://www.formula1.com/',
+                            'Referer': 'https://www.formula1.com/',
+                        }
+                    })
+                    console.log(res.data);
+            }
+    
+            catch (error) {
+                console.log(error);
+            }
+        }
+
+        async function DecideWhatSourceToGetTiming() {
+            let WebsocketStatus = await CheckWebsocketOpen();
+
+            switch (WebsocketStatus.Status) {
+                case "Offline":
+                    GetRaceResult();
+                    console.log("Websocket closed.");
+                    break;
+                
+                case "Available":
+                    console.log("Livetiming available.");
+                    break;
+                
+                default:
+                    console.log("Something is wrong with StreamingStatus.json!");
+                    break;
+            }
+
+            
+        }
+
+
+
+/*__________________________________________________________________________________________*/
+        
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    happystuff();
+});
